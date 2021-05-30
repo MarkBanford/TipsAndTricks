7d8133f2509a69cdbabd1ceb2594be21b6960a41
markb <markbanford@gmail.com>
2021-05-30 20:51:20 +0100

    Advanced

--- PythonTricks/Decorators.py
+++ PythonTricks/Decorators.py
@@ -1,16 +1,22 @@
+# we want to take a list of strings and camelcase them. mapper will allow us
+# to use our camelcase function to a list of strings
+
 def mapper(fnc):
     def inner(list_of_values):
-        return [fnc(val) for val in list_of_values]
+        return [fnc(value) for value in list_of_values]
 
     return inner
 
 
 @mapper
 def camelcase(s):
-    '''Turns strings_like_this into StringsLikeThis'''
+    """Turns into StringsLikeThis"""
     return ''.join([word.capitalize() for word in s.split('_')])
 
 
-names = ['rick_ross', 'snoop_dogg']
+names = [
+    'rick_ross',
+    'snoop_dogg'
+]
 
 print(camelcase(names))
