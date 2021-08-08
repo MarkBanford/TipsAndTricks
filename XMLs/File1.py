32ffdbba8e40d1d8b786ed525b034fb7ac5c97af
Mark Banford <markbanford@gmail.com>
2021-08-08 21:30:34 +0100

    1

--- XMLs/File1.py
+++ XMLs/File1.py
@@ -1,4 +1,2 @@
 import pandas as pd
-import pandas_read_xml as pdx
-
-data = pdx.read_xml('https://www.sec.gov/Archives/edgar/data/1000351/000114554921012283/primary_doc.xml', ['edgarSubmission'])
+print(pd.__version__)
