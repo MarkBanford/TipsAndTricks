33e06c317217d5c92b45e5ef15d664d2ea786c75
markb <markbanford@gmail.com>
2022-05-29 19:50:37 +0100

    Merge remote-tracking branch 'origin/master'

--- PythonTricks/Properties.py
+++ PythonTricks/Properties.py
@@ -1,1 +1,22 @@
-import os
+class Customer:
+    def __init__(self, name='a', membership_type='b'):
+        self.name = name
+        self.membership_type = membership_type
+
+    def __repr__(self):
+        return f"Customer('{self.name}','{self.membership_type})"
+
+
+names = ['John', 'Paul', 'Simon']
+memb = ['Bronze', 'Silver', 'Gold']
+
+instances = [Customer() for i in range(len(names))]
+
+for i in range(len(names)):
+    instances[i].name = names[i]
+    instances[i].membership_type = memb[i]
+
+
+
+
+
