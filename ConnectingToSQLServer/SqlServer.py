7df52a4c91b4db29369afb23af6ef5daef4b898a
markb <markbanford@gmail.com>
2022-07-24 16:41:19 +0100

    1

--- ConnectingToSQLServer/SqlServer.py
+++ ConnectingToSQLServer/SqlServer.py
@@ -7,8 +7,45 @@ conn = pyodbc.connect("DRIVER={SQL Server};"
 
 cursor = conn.cursor()
 
-cursor.execute("""SELECT *
-  FROM [AdventureWorks2012].[Person].[Person]""")
+TABLE_NAME = 'TableTester'
 
-for row in cursor:
-    print(f'row={row}')
+
+def create(conn):
+    print("Creating Table")
+    cursor = conn.cursor()
+    cursor.execute(""" USE AdventureWorks2012 CREATE TABLE Tester2
+    (PersonID int,
+    LastName varchar(50),
+    FirstName varchar(25),
+    Age tinyint);""")
+    conn.commit()
+
+
+def read(conn):
+    print("Reading Table")
+    cursor = conn.cursor()
+    cursor.execute("""SELECT *
+  FROM [AdventureWorks2012].[dbo].[Tester2]""")
+    for row in cursor:
+        print(f'row={row}')
+    print()
+
+
+def update(conn):
+    print("Updating Table")
+    cursor = conn.cursor()
+    cursor.execute(""" UPDATE[AdventureWorks2012].[dbo].[Tester2]
+    SET LastName='Thomas'
+    WHERE LastName='Smith'""")
+    conn.commit()
+
+
+def delete(conn):
+    print('Delete')
+    cursor = conn.cursor()
+    cursor.execute(""" DELETE FROM [AdventureWorks2012].[dbo].[Tester2]
+    WHERE LastName='LN2'
+    """)
+    conn.commit()
+
+
